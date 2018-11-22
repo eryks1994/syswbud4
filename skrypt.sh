b371658157d26efb3d2fb7c2b4951f165415c693
rzyrzek <32631135+rzyrzek@users.noreply.github.com>
2018-11-22 12:42:03 +0100

    poprawki_v1

--- skrypt.sh
+++ skrypt.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 LED_GPIO=0
 
-if [ $1 == "on" ]
+if [ $1 == "on" ];
 	then
 	echo "LED on"
 	echo 200>>"/sys/class/leds/led$LED_GPIO/brightness"
-elif[ $1 == "off" ]
+elif[ $1 == "off" ];
 	then
 	echo "LED off"
 	echo 0>>"/sys/class/leds/led$LED_GPIO/brightness"
-elif[ $1 == "blink" ]
+elif[ $1 == "blink" ];
 	then
 	echo "LED blink"
 	echo time>>"/sys/class/leds/led$LED_GPIO/trigger"
